df288e79647ea1b3d8bfcdf48d81d49790f7cc88
Md Najmul Haque <najmulhaque008@gmail.com>
2022-12-04 15:07:53 +0600

    successfully connected the database

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 import app from './app.js'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+import colors from 'colors'
 
 const port = process.env.PORT || 5000
 
+//database connection
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log(`Database connection is successful`.green.bold)
+})
+
 app.listen(port, () => {
-    console.log(`port is listening to the ${port}`)
+    console.log(`port is listening to the ${port}`.blue.bold)
 })
