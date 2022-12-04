dd130e7574a1c8ffe0de2860e186567f2018591a
Md Najmul Haque <najmulhaque008@gmail.com>
2022-12-05 00:05:09 +0600

    save car controller updated

--- controllers/carController.js
+++ controllers/carController.js
@@ -3,22 +3,23 @@ import Car from "../model/carSchema.js"
 export const saveCar = async (req, res, next) => {
 
     try {
+        // const car = req.body
+        // const result = await Car.create(car)
+
         const car = new Car(req.body)
         const result = await car.save()
+
         res.status(200).json({
             status: 'success',
             message: 'Car data saved successfully',
             result
-
         })
-
 
     } catch (error) {
         res.status(400).json({
             status: 'fail',
             message: 'Fail to save car data',
             error: error.message,
-
         })
     }
 
